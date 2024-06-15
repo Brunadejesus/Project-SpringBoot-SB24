b7350e4ba4a60ff41d30ad85bb601f250cbddef8
Brunadejesus <brunadejesus.dev@gmail.com>
2024-06-15 20:41:44 -0300

    Primeira etapa concluida 20% ====

---
+++ Project-SB24/src/test/java/dev/project/ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSantanderBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
